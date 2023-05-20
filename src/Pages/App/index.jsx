2a2104ac39a53340869adfcf6bc94e6557b6eb81
Gilbert Ardila <gilbertferney@gmail.com>
2023-05-20 18:06:36 -0500

    main structure done

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,9 +1,15 @@
+import Home from "../Home";
+import Crud from "../Crud";
+import Employees from "../Employees";
+import Ubications from "../Ubications";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="bg-red-100">
+        <Home />
         <h1>Hola mundo</h1>
       </div>
     </>
---
+++ src/Pages/Ubications/index.jsx
@@ -0,0 +1,8 @@
+
+const Ubications = () => {
+  return (
+    <div>Ubications</div>
+  )
+}
+
+export default Ubications
